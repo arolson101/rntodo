e78f8cc42060e347c1f5fec9b9a6bbc88cb7f53c
Andrew Olson <andrew@olsononline.org>
2024-08-27 12:55:59 -0500

    react-native-web with vite

---
+++ components/web/NewAppScreen/index.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import Header from './Header.web';
+import LearnMoreLinks from './LearnMoreLinks.web';
+import Colors from './Colors.web';
+import DebugInstructions from './DebugInstructions.web';
+import ReloadInstructions from './ReloadInstructions.web';
+
+export {Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions};
---
+++ vite.config.ts
@@ -0,0 +1,59 @@
+// https://stereobooster.com/posts/react-native-web-with-vite/#final-config
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+// import { esbuildFlowPlugin } from "@bunchtogether/vite-plugin-flow";
+
+// https://tamagui.dev/docs/intro/installation
+const extensions = [
+  '.web.tsx',
+  '.tsx',
+  '.web.ts',
+  '.ts',
+  '.web.jsx',
+  '.jsx',
+  '.web.js',
+  '.js',
+  '.css',
+  '.json',
+  '.mjs',
+];
+
+const development = process.env.NODE_ENV === 'development';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  clearScreen: true,
+  plugins: [react()],
+  define: {
+    // https://github.com/bevacqua/dragula/issues/602#issuecomment-1296313369
+    global: 'window',
+    __DEV__: JSON.stringify(development),
+    // https://tamagui.dev/docs/intro/installation
+    DEV: JSON.stringify(development),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  resolve: {
+    extensions: extensions,
+    alias: [
+      {
+        find: 'react-native/Libraries/NewAppScreen',
+        replacement: './components/web/NewAppScreen/index.js',
+      },
+      {find: 'react-native', replacement: 'react-native-web'},
+    ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      // https://github.com/vitejs/vite-plugin-react/issues/192#issuecomment-1627384670
+      jsx: 'automatic',
+      // need either this or the plugin below
+      loader: {'.js': 'jsx'},
+      // plugins: [
+      //   esbuildFlowPlugin(/\.(flow|jsx?)$/, (path) =>
+      //     /\.jsx$/.test(path) ? "jsx" : "jsx"
+      //   ),
+      // ],
+    },
+  },
+});

7e0373fbe96f601e7799100956cde515eb3a7ff2
Andrew Olson <andrew@olsononline.org>
2024-08-28 11:49:35 -0500

    typescript

--- app/index.web.ts
+++ app/index.web.ts
@@ -1,10 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import {name as appName} from '../app.json';
-import './index.js';
+import './index.ts';
 
 AppRegistry.runApplication(appName, {
   rootTag: document.getElementById('root'),

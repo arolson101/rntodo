1953ae8ef02b59ad98da40d1739fd08aa208a906
Andrew Olson <andrew@olsononline.org>
2024-08-30 12:49:53 -0500

    fix react-native

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type {Config} from 'tailwindcss';
 export default {
   // darkMode: 'class',
   content: ['./index.*', './app/**/*.{js,jsx,ts,tsx}'],
-  presets: [[require('nativewind/preset')]],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
